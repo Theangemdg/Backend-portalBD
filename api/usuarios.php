823fe82b6b4ff335c5d224ad85e05aa373ebc2dc
Rick <ricky95hnd@gmail.com>
2022-08-17 17:52:50 -0600

    portal-usuarios-api finalizado

--- api/usuarios.php
+++ api/usuarios.php
@@ -1,29 +1,30 @@
 <?php
 
-    include_once("../clases/class-usuarios.php");
-    header("Content-Type: application/json");
-    switch($_SERVER['REQUEST_METHOD']){
-        case 'POST':
-            $_POST = json_decode(file_get_contents('php://input'),true);
-            $usuario = new Usuario($_POST["nombre"], $_POST["correo"], $_POST["contrasena"], $_POST["direccion"], $_POST["ordenes"], $_POST["pedidos"], $_POST["metodoPago"]);
-            $usuario -> guardarUsuarios();
-            $resultado["mensaje"] = "Guardar usuiario, informacion: ".json_encode($_POST);
-            echo json_encode($resultado); 
+include_once("../clases/class-usuarios.php");
+header("Content-Type: application/json");
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        $usuario = new Usuario($_POST["id_usuario"], $_POST["nombre"], $_POST["apellido"], $_POST["edad"], $_POST["telefono"], $_POST["contraseña"], $_POST["correo"]);
+        $usuario->guardarUsuarios();
+        $resultado["mensaje"] = "Guardar usuario, informacion: " . json_encode($_POST);
+        echo json_encode($resultado);
         break;
-        case 'GET':
-            if (isset($_GET['id'])){
-                Usuario::obtenerUsuario($_GET['id']);
-            }else{
-                Usuario::obtenerUsuarios();
-            }
+    case 'GET':
+        if (isset($_GET['id'])) {
+            Usuario::obtenerUsuario($_GET['id']);
+        }
+        else {
+            Usuario::obtenerUsuarios();
+        }
         break;
-        case 'PUT':
+    case 'PUT':
 
         break;
-        case 'DELETE':
+    case 'DELETE':
 
         break;
-    }
+}
 
 ?>
 
--- clases/class-usuarios.php
+++ clases/class-usuarios.php
@@ -1,59 +1,90 @@
 <?php
 
 
-    class Usuario {
+class Usuario
+{
+
+        private $id_usuario;
         private $nombre;
+        private $apellido;
+        private $edad;
+        private $telefono;
+        private $contraseña;
         private $correo;
-        private $contrasena;
-        private $direccion;
-        private $ordenes;
-        private $pedidos;
-        private $metodoPago;
 
-        public function __construct($nombre,$correo,$contrasena,$direccion,$ordenes,$pedidos,$metodoPago){
-            $this->nombre = $nombre;
-            $this->correo = $correo;
-            $this->contrasena = $contrasena;
-            $this->direccion = $direccion;
-            $this->ordenes = $ordenes;
-            $this->pedidos = $pedidos;
-            $this->metodoPago = $metodoPago;
-
+        public function __construct($id_usuario, $nombre, $apellido, $edad, $telefono, $contraseña, $correo)
+        {
+                $this->id_usuario = $id_usuario;
+                $this->nombre = $nombre;
+                $this->apellido = $apellido;
+                $this->edad = $edad;
+                $this->telefono = $telefono;
+                $this->contraseña = $contraseña;
+                $this->correo = $correo;
         }
 
-        public function guardarUsuarios(){
-            $contenidoArchivo =  file_get_contents("../data/usuarios.json");
-            $usuarios = json_decode($contenidoArchivo, true);
-            $usuarios[] = array(
-                    "nombre"=> $this->nombre,
-                    "correo"=> $this->correo,
-                    "contrasena"=> $this->contrasena,
-                    "direccion"=> $this->direccion,
-                    "ordenes"=> $this ->ordenes,
-                    "pedidos"=> $this ->pedidos,
-                    "metodoPago"=> $this ->metodoPago,
+        public function guardarUsuarios()
+        {
+                $conexion = new PDO("sqlsrv:server=localhost;database=Portal", "admin", "portal");
+                $consulta = $conexion->prepare("insert into portal.usuarios
+                values ('$this->id_usuario','$this->nombre' ,'$this->apellido', '$this->edad','$this->telefono','$this->contraseña','$this->correo')");
+                $consulta->execute();
 
-            );
-            $archivo = fopen("../data/usuarios.json","w");
-            fwrite($archivo, json_encode($usuarios));
-            fclose($archivo);
+                $conexion = null;
+                $consulta = null;
         }
 
-        public static function obtenerUsuario($indice){
-            $contenidoArchivo =  file_get_contents("../data/usuarios.json");
-            $usuarios = json_decode($contenidoArchivo, true);
-            echo json_encode($usuarios[$indice]);
+        public static function obtenerUsuario($indice)
+        {
+                $conexion = new PDO("sqlsrv:server=localhost;database=Portal", "admin", "portal");
+                $consulta = $conexion->prepare("SELECT * from portal.usuarios where id_usuario = $indice");
+                $consulta->execute();
+
+                $datos = $consulta->fetchAll(PDO::FETCH_OBJ);
+                $conexion = null;
+                $consulta = null;
+                $categoria = json_encode($datos[0]);
+                echo $categoria;
         }
 
-        public static function obtenerUsuarios(){
-                $contenidoArchivo =  file_get_contents("../data/usuarios.json");
-                echo $contenidoArchivo ;
+        public static function obtenerUsuarios()
+        {
+                $conexion = new PDO("sqlsrv:server=localhost;database=Portal", "admin", "portal");
+                $consulta = $conexion->prepare("SELECT * from portal.usuarios");
+                $consulta->execute();
+
+                $datos = $consulta->fetchAll(PDO::FETCH_OBJ);
+                echo json_encode($datos);
+                $conexion = null;
+                $consulta = null;
         }
-        
+
+
+
+
+        /**
+         * Get the value of id_usuario
+         */
+        public function getId_usuario()
+        {
+                return $this->id_usuario;
+        }
+
+        /**
+         * Set the value of id_usuario
+         *
+         * @return  self
+         */
+        public function setId_usuario($id_usuario)
+        {
+                $this->id_usuario = $id_usuario;
+
+                return $this;
+        }
 
         /**
          * Get the value of nombre
-         */ 
+         */
         public function getNombre()
         {
                 return $this->nombre;
@@ -63,7 +94,7 @@
          * Set the value of nombre
          *
          * @return  self
-         */ 
+         */
         public function setNombre($nombre)
         {
                 $this->nombre = $nombre;
@@ -72,8 +103,88 @@
         }
 
         /**
+         * Get the value of apellido
+         */
+        public function getApellido()
+        {
+                return $this->apellido;
+        }
+
+        /**
+         * Set the value of apellido
+         *
+         * @return  self
+         */
+        public function setApellido($apellido)
+        {
+                $this->apellido = $apellido;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of edad
+         */
+        public function getEdad()
+        {
+                return $this->edad;
+        }
+
+        /**
+         * Set the value of edad
+         *
+         * @return  self
+         */
+        public function setEdad($edad)
+        {
+                $this->edad = $edad;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of telefono
+         */
+        public function getTelefono()
+        {
+                return $this->telefono;
+        }
+
+        /**
+         * Set the value of telefono
+         *
+         * @return  self
+         */
+        public function setTelefono($telefono)
+        {
+                $this->telefono = $telefono;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of contraseña
+         */
+        public function getContraseña()
+        {
+                return $this->contraseña;
+        }
+
+        /**
+         * Set the value of contraseña
+         *
+         * @return  self
+         */
+        public function setContraseña($contraseña)
+        {
+                $this->contraseña = $contraseña;
+
+                return $this;
+        }
+
+        /**
          * Get the value of correo
-         */ 
+         */
         public function getCorreo()
         {
                 return $this->correo;
@@ -83,112 +194,12 @@
          * Set the value of correo
          *
          * @return  self
-         */ 
+         */
         public function setCorreo($correo)
         {
                 $this->correo = $correo;
 
                 return $this;
         }
-
-        /**
-         * Get the value of contrasena
-         */ 
-        public function getContrasena()
-        {
-                return $this->contrasena;
-        }
-
-        /**
-         * Set the value of contrasena
-         *
-         * @return  self
-         */ 
-        public function setContrasena($contrasena)
-        {
-                $this->contrasena = $contrasena;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of ordenes
-         */ 
-        public function getOrdenes()
-        {
-                return $this->ordenes;
-        }
-
-        /**
-         * Set the value of ordenes
-         *
-         * @return  self
-         */ 
-        public function setOrdenes($ordenes)
-        {
-                $this->ordenes = $ordenes;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of pedidos
-         */ 
-        public function getPedidos()
-        {
-                return $this->pedidos;
-        }
-
-        /**
-         * Set the value of pedidos
-         *
-         * @return  self
-         */ 
-        public function setPedidos($pedidos)
-        {
-                $this->pedidos = $pedidos;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of metodoPago
-         */ 
-        public function getMetodoPago()
-        {
-                return $this->metodoPago;
-        }
-
-        /**
-         * Set the value of metodoPago
-         *
-         * @return  self
-         */ 
-        public function setMetodoPago($metodoPago)
-        {
-                $this->metodoPago = $metodoPago;
-
-                return $this;
-        }
-
-        /**
-         * Get the value of direccion
-         */ 
-        public function getDireccion()
-        {
-                return $this->direccion;
-        }
-
-        /**
-         * Set the value of direccion
-         *
-         * @return  self
-         */ 
-        public function setDireccion($direccion)
-        {
-                $this->direccion = $direccion;
-
-                return $this;
-        }
-    }
+}
 ?>
