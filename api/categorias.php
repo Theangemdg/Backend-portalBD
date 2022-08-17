48fedc8e3839a4b0b03a19fa3250f260b404a582
A. kamisato <101378478+Theangemdg@users.noreply.github.com>
2022-08-17 16:49:50 -0600

    portal-categorias-api finalizado

--- api/categorias.php
+++ api/categorias.php
@@ -5,7 +5,7 @@
     switch($_SERVER['REQUEST_METHOD']){
         case 'POST':
             $_POST = json_decode(file_get_contents('php://input'),true);
-            $categoria = new Categoria($_POST["id"], $_POST["nombreCategoria"], $_POST["icono"], $_POST["productos"]);
+            $categoria = new Categoria($_POST["id_categoria"], $_POST["nombreCategoria"], $_POST["descripcion"], $_POST["cantidad_productos"], $_POST["icono"]);
             $categoria -> guardarCategoria();
             $resultado["mensaje"] = "Guardar categoria, informacion: ".json_encode($_POST);
             echo json_encode($resultado); 
@@ -19,15 +19,10 @@
         break;
         case 'PUT':
             $_PUT = json_decode(file_get_contents('php://input'),true);
-            $categoria = new Categoria($_PUT['id'], $_PUT['nombreCategoria'], $_PUT['icono'], $_PUT['productos']);
+            $categoria = new Categoria($_PUT["id_categoria"], $_PUT["nombreCategoria"], $_PUT["descripcion"], $_PUT["cantidad_productos"], $_PUT["icono"]);
             $categoria->actualizarCategoria($_GET['id']);
             $resultado["mensaje"] = "Actualizar una categoria con el id: ".$_GET['id'].
                                     ", Informacion a actualizar: ".json_encode($_PUT);
-            echo json_encode($resultado);
-        break;
-        case 'DELETE':
-            Categoria::eliminarCategoria($_GET["id"]);
-            $resultado["mensaje"] = "Eliminar una categoria con el id: ".$_GET['id'];
             echo json_encode($resultado);
         break;
     }
