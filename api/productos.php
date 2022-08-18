58d5379320f652631733869c4a61d020726ed1e5
A. kamisato <101378478+Theangemdg@users.noreply.github.com>
2022-08-17 18:54:56 -0600

    portal-producto-api finalizado

--- api/productos.php
+++ api/productos.php
@@ -1,35 +1,33 @@
 <?php
 
-    include_once("../clases/class-productos.php");
-    header("Content-Type: application/json");
-    switch($_SERVER['REQUEST_METHOD']){
-        case 'POST':
-            $_POST = json_decode(file_get_contents('php://input'),true);
-            $producto = new Producto($_POST["id"], $_POST["nombreProducto"], $_POST["imgProducto"], $_POST["descripcion"], $_POST["precio"]);
-            $producto -> guardarProducto($_GET['id']);
-            $resultado["mensaje"] = "Guardar producto, informacion: ".json_encode($_POST);
-            echo json_encode($resultado); 
+include_once "../clases/class-productos.php";
+header("Content-Type: application/json");
+switch ($_SERVER['REQUEST_METHOD']) {
+    case 'POST':
+        $_POST = json_decode(file_get_contents('php://input'), true);
+        $producto = new Producto($_POST["id_producto"], $_POST["nombre"], $_POST["descripcion"], $_POST["precio"], $_POST["id_categoria"], $_POST["imagen"]);
+        $producto->guardarProducto($_GET['id']);
+        $resultado["mensaje"] = "Guardar producto, informacion: " . json_encode($_POST);
+        echo json_encode($resultado);
         break;
-        case 'GET':
-            if (isset($_GET['id']) && isset($_GET['idP'])){
-                Producto::obtenerProducto($_GET['id'],$_GET['idP']);
-            }else if (isset($_GET['id'])){
-                Producto::obtenerProductos($_GET['id']);
-            }
+    case 'GET':
+        if (isset($_GET['id']) && isset($_GET['idP'])) {
+            Producto::obtenerProducto($_GET['id'], $_GET['idP']);
+        } else if (isset($_GET['id'])) {
+            Producto::obtenerProductos($_GET['id']);
+        }
         break;
-        case 'PUT':
-            $_PUT = json_decode(file_get_contents('php://input'),true);
-            $producto = new Producto($_PUT["id"], $_PUT["nombreProducto"], $_PUT["imgProducto"], $_PUT["descripcion"], $_PUT["precio"]);
-            $producto->actualizarProducto($_GET['id'], $_GET['idP']);
-            $resultado["mensaje"] = "Actualizar un producto con el id: ".$_GET['id'].
-                                    ", Informacion a actualizar: ".json_encode($_PUT);
-            echo json_encode($resultado);
-        break;  
-        case 'DELETE':
-            Producto::eliminarProducto($_GET["id"],$_GET['idP']);
-            $resultado["mensaje"] = "Eliminar un producto con el id: ".$_GET['idP'];
-            echo json_encode($resultado);
+    case 'PUT':
+        $_PUT = json_decode(file_get_contents('php://input'), true);
+        $producto = new Producto($_PUT["id_producto"], $_PUT["nombre"], $_PUT["descripcion"], $_PUT["precio"], $_PUT["id_categoria"], $_PUT["imagen"]);
+        $producto->actualizarProducto($_GET['id'], $_GET['idP']);
+        $resultado["mensaje"] = "Actualizar un producto con el id: " . $_GET['idP'] .
+        ", Informacion a actualizar: " . json_encode($_PUT);
+        echo json_encode($resultado);
         break;
-    }
-
-?>
+    case 'DELETE':
+        Producto::eliminarProducto($_GET["id"], $_GET['idP']);
+        $resultado["mensaje"] = "Eliminar un producto con el id: " . $_GET['idP'];
+        echo json_encode($resultado);
+        break;
+}
